d9c0df04d404244f53bca95fb1de4d9775afd560
Aytchinda <aymardrinel@outlook.fr>
2024-01-17 17:41:35 +0100

    modifications routes.yaml; LoginController.php, templates/login, templates/partials et creation map.html.twig

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,6 +12,11 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // Si l'utilisateur est déjà connecté, rediriger vers la page de profil
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
@@ -19,5 +24,12 @@ class LoginController extends AbstractController
             'error' => $error,
             'last_username' => $lastUsername,
         ]);
+    }
+
+    
+    #[Route('/app_logout', name: 'app_logout')]
+    public function logout()
+    {
+        // La route de déconnexion ne nécessite pas de code, Symfony gère cela automatiquement
     }
 }
